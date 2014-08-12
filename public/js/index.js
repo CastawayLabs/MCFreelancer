6651d40884c7053dc3c9dd0fed27a5ba70401ca6
Zia Ur Rehman <zia@brainloggers.co.uk>
2014-08-13 00:20:13 +0500

    refactor(index.jade): new layout for the front page
    * stock image from unsplash.com
    * carousel-featured no longer has separate opaque backgrounds
    * extra: tagline/slogan, project description etc on front page
    partially completed
    
    TODO: stats-mini, scrollspy, how-mini etc.

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,16 @@ jQuery(document).ready(function ($) {
 
   'use strict';
 
+  // Since our main-section background is greyish white, we oughta
+  // make our navbar transparent.
+  // A class has been created in index.less which makes the navbar
+  // transparent and makes other color adjustments to the elements
+  // contained.
+  $('#navbar-main').addClass('txp');
+
+  // TODO: remove the txp class upon scroll-past main-section
+  //       will need an elaborate scrollspy for that
+
   // This is stupid. We shouldn't have to do this!
   $('[data-toggle="popover"]').popover();
 
